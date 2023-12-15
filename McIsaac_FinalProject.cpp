2891e2ba08472e463bdae7035755adef73c02c8c
emmcisaa <123347220+emmcisaa@users.noreply.github.com>
2023-12-15 13:57:08 -0600

    EM Done with Fina Project!

--- McIsaac_FinalProject.cpp
+++ McIsaac_FinalProject.cpp
@@ -3,64 +3,140 @@
 // Author: Erin M. McIsaac
 // Date Last Modified: 12/12/2023
 
+// When running this file, please remember to only run in OpenGL !!! Otherwise, it won't run!
+
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
-int main() 
-{
-    ifstream inFile;
-    ofstream outFile;
-    char inName[200];
-    char outName[] = "out.txt";  // Output file name is fixed
-    char input;
-    int direction;
+const int MAX_WORDS = 20000;
 
-    // Enter the input file
-    cout << "Enter the input file name (i.e., English.txt): ";
-    cin >> inName;
-    inFile.open(inName);
+struct Translation {
+    string english;
+    string spanish;
+};
 
-
-    // Enter the output file
-    cout << "Output will be written to: " << outName << endl;
-    outFile.open(outName);
-
-    // Testing inFile (for failure)
-    if (!inFile) 
-    {
-        cout << "Error opening " << inName << endl;
-        return 2;
+void readTranslations(Translation translations[], int& size) {
+    ifstream inFile("Translations.txt");
+    if (!inFile) {
+        cout << "Error opening Translations.txt" << endl;
+        exit(1);
     }
 
-    // Testing outFile (for failure)
-    if (!outFile) 
-    {
-        cout << "Error opening " << outName << endl;
-        return 6;
+    size = 0;
+    while (inFile >> translations[size].english >> translations[size].spanish) {
+        size++;
     }
 
-    cout << "Enter 1 to translate to Spanish. Enter 2 to translate to English." << endl;
-    cin >> direction;
-
-    while (inFile.get(input)) 
-    {
-        if (direction == 1) 
-        {
-            // Translate to Spanish (example: shift characters by 1 positions)
-            input += 1;
-        } 
-        else 
-        {
-            // Translate to English (example: shift characters back by 1 positions)
-            input -= 1;
-        }
-        outFile.put(input);
-    }
-
-    cout << "The file " << outName << " has been processed." << endl;
     inFile.close();
+}
+
+void writeTranslations(const Translation translations[], int size) {
+    ofstream outFile("Translations.txt");
+    if (!outFile) {
+        cout << "Error opening Translations.txt for writing" << endl;
+        exit(1);
+    }
+
+    for (int i = 0; i < size; ++i) {
+        outFile << translations[i].english << " " << translations[i].spanish << endl;
+    }
+
     outFile.close();
+}
+
+void addWord(Translation translations[], int& size) {
+    if (size < MAX_WORDS) {
+        cout << "Enter English Word: ";
+        cin >> translations[size].english;
+        cout << "Enter Spanish Translation: ";
+        cin >> translations[size].spanish;
+
+        size++;
+        cout << "Thank you for adding a new word to our dictionary!" << endl;
+    } else {
+        cout << "Dictionary is full. Cannot add more words." << endl;
+    }
+}
+
+string translate(const string& word, const Translation translations[], int size) {
+    for (int i = 0; i < size; ++i) {
+        if (translations[i].english == word) {
+            return translations[i].spanish;
+        } else if (translations[i].spanish == word) {
+            return translations[i].english;
+        }
+    }
+
+    return word; // Return the original word if translation not found
+}
+
+void exitTranslator() {
+    cout << "Thank you for using the translator! Have a nice day." << endl;
+}
+
+int main() {
+    cout << "Welcome to the Language Translator! Press Enter to start." << endl;
+    cout << "---------------------------------------------------------" << endl;
+    cin.get(); // Wait for a key press
+
+    Translation translationDictionary[MAX_WORDS];
+    int dictionarySize = 0;
+    readTranslations(translationDictionary, dictionarySize);
+
+    bool continueTranslation = true;
+
+    while (continueTranslation) {
+        cin.ignore(); // Clear newline from previous input
+        string input;
+        cout << "Enter a sentence for translation: ";
+        getline(cin, input);
+
+        istringstream iss(input);
+        string word;
+
+        while (iss >> word) {
+            string translation = translate(word, translationDictionary, dictionarySize);
+            cout << translation << " ";
+            if (translation == word) {
+                cout << "Word not recognized!" << endl;
+                cout << "\nWould you like to add a new word to the dictionary? Press 1 for yes, 2 for no: ";
+                char choice;
+                cin >> choice;
+                if (choice == '1') {
+                    addWord(translationDictionary, dictionarySize);
+                    writeTranslations(translationDictionary, dictionarySize);
+                } else if (choice == '2') {
+                    exitTranslator();
+                    continueTranslation = false;
+                    break; // Exit the loop
+                }
+            }
+        }
+
+        cout << endl;
+
+        if (continueTranslation) {
+            cout << "Would you like to translate another sentence?" << endl;
+            cout << "1) Yes" << endl;
+            cout << "2) No" << endl;
+            cout << "Enter your choice: ";
+            int menuChoice;
+            cin >> menuChoice;
+
+            switch (menuChoice) {
+                case 1:
+                    break; // Continue translation (no action needed)
+                case 2:
+                    exitTranslator();
+                    continueTranslation = false;
+                    break; // Exit the loop
+            }
+        }
+    }
 
     return 0;
 }
